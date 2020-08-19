ae67bc65b302fd8647edd02bbd186cd654e22867
Alex Popov <yolloartstudio@gmail.com>
2020-08-19 17:02:08 +0300

    initial commit

---
+++ TwoRectangles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoRectangles
+//
+//  Created by YolloArts on 19.08.2020.
+//  Copyright © 2020 YolloArts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
